49bb66b4fd68e6be11be378bff5f63f74fc43c8a
esblinov <esblinov@avito.ru>
2025-10-14 18:18:31 +0300

    Add test for Python 3.13+ type‑check behavior

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -446,6 +446,9 @@ def test_more_examples_of_type_check_when_redefine_defaults_initing_new_object_f
     if sys.version_info < (3, 10):
         with pytest.raises(AttributeError):
             SomeClass(field='kek')
+    elif sys.version_info > (3, 13):
+        with pytest.raises(TypeError, match=match('The value "kek" (str) of the "field" field does not match the type Union.')):
+            SomeClass(field='kek')
     else:
         with pytest.raises(TypeError, match=match('The value "kek" (str) of the "field" field does not match the type Optional.')):
             SomeClass(field='kek')
