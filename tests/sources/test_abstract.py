3ee8213acd8a06e9812f046420634a11d2d0cb00
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-10-28 02:58:04 +0300

    Fix test to not assert exact error message

--- tests/sources/test_abstract.py
+++ tests/sources/test_abstract.py
@@ -1,9 +1,8 @@
 import pytest
-from full_match import match
 
 from skelet.sources.abstract import AbstractSource
 
 
 def test_cant_instantiate_abstract_class():
-    with pytest.raises(TypeError, match=match("Can't instantiate abstract class AbstractSource with abstract method __getitem__")):
+    with pytest.raises(TypeError):
         AbstractSource()
