74fd534e2d80730c5196bb8402bcedd7e5a519f3
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-11-07 05:44:55 +0300

    Fix getter_for_libraries return type annotation

--- skelet/sources/getter_for_libraries.py
+++ skelet/sources/getter_for_libraries.py
@@ -5,9 +5,4 @@ from skelet import EnvSource, TOMLSource, JSONSource, YAMLSource
 
 
 def for_tool(tool_name: str) -> List[AbstractSource]:
-    return (
-        EnvSource.for_library(tool_name)
-        + TOMLSource.for_library(tool_name)
-        + YAMLSource.for_library(tool_name)
-        + JSONSource.for_library(tool_name)
-    )
+    return EnvSource.for_library(tool_name) + TOMLSource.for_library(tool_name) + YAMLSource.for_library(tool_name) + JSONSource.for_library(tool_name)  # type: ignore[return-value, operator]
